c2c7c7e5258829740550df415e321e55bfa09dbd
Ted <Ted Carter@.(none)>
2011-04-05 20:45:33 -0400

    ships movement

--- spaceconquest/Game Structures/Ship.cs
+++ spaceconquest/Game Structures/Ship.cs
@@ -12,32 +12,52 @@ using Microsoft.Xna.Framework.Media;
 namespace spaceconquest
 {
     [Serializable]
-    class Ship:Unit
+    class Ship : Unit
     {
-        
+
         protected int speed = 8;
-        [NonSerialized] protected Hex3D ghosthex;
-        [NonSerialized] protected LineModel line;
-        [NonSerialized] protected ShipModel shipmodel;
+        [NonSerialized]
+        protected Hex3D ghosthex;
+        [NonSerialized]
+        protected LineModel line;
+        [NonSerialized]
+        protected ShipModel shipmodel;
         protected float hoveringHeight = 7;
         protected float hoveringAcc = -0.06f;
         protected String modelstring = "starcruiser";
 
         protected double targetangle = 0;
         protected double currentAngle = 0;
-        protected Vector3 oldposition = new Vector3(0,0,0);
+        protected Vector3 oldposition = new Vector3(0, 0, 0);
         protected Queue<Vector3> targetpositions = new Queue<Vector3>();
-        protected Vector3 targetvector = new Vector3(0,0,0);
+        protected Queue<double> targetangles = new Queue<double>();
+        protected Vector3 targetvector = new Vector3(0, 0, 0);
         private int percenttraveled = 0;
+        private int percentturned = 0;
 
-        public void move(Hex3D target) {
+        public void move(Hex3D target)
+        {
             hex.RemoveObject();
             //oldposition = hex.getCenter();
             //percenttraveled = 0;
-            SetHex(target);
-            if (ghosthex!=null) ghosthex.ClearGhostObject();
+
+            if (ghosthex != null) ghosthex.ClearGhostObject();
             line = null;
             targetpositions.Enqueue(target.getCenter());
+
+
+
+            double x = target.getCenter().X - hex.getCenter().X;
+            double y = hex.getCenter().Y - target.getCenter().Y;
+            double newangle = Math.Atan(x / y);
+            Console.WriteLine(" x  " + x);
+            Console.WriteLine(" y " + y);
+            if (y < 0) newangle = newangle + Math.PI;
+            Console.WriteLine("angle -> " + newangle);
+            targetangles.Enqueue(newangle);
+
+            SetHex(target);
+
         }
 
         public Ship(String s)
@@ -46,7 +66,8 @@ namespace spaceconquest
         }
 
 
-        public int getSpeed() { 
+        public int getSpeed()
+        {
             return speed;
         }
 
@@ -71,30 +92,34 @@ namespace spaceconquest
         {
             List<Hex3D> hexes = reachable(hex, speed);
             hexes.Add(hex);
-            foreach (Hex3D h in hexes) {
+            foreach (Hex3D h in hexes)
+            {
                 h.distance = -1;
-                if (h.GetGameObject() != null) {
+                if (h.GetGameObject() != null)
+                {
                     //hexes.Remove(h);
                 }
             }
-            hex.distance = -1; 
+            hex.distance = -1;
             return hexes;
         }
 
         List<Hex3D> reachable(Hex3D startHex, int r)
-        {   
+        {
             startHex.distance = r;
             List<Hex3D> hexes = new List<Hex3D>();
-            if (r <= 0) {
+            if (r <= 0)
+            {
                 return hexes;
             }
             foreach (Hex3D h in startHex.getNeighbors())
             {
                 //if (!h.passable) continue;
                 int dist = h.distance;
-                if (dist == -1 || dist < r-1) {
+                if (dist == -1 || dist < r - 1)
+                {
                     hexes.Add(h);
-                    hexes.AddRange(reachable(h, r-1));
+                    hexes.AddRange(reachable(h, r - 1));
                 }
             }
 
@@ -113,7 +138,7 @@ namespace spaceconquest
             if (y < 0) targetangle = targetangle + Math.PI;
             Console.WriteLine("angle -> " + targetangle);
 
-
+            if (ghosthex != null) ghosthex.SetGhostObject(null);
             ghosthex = target;
             target.SetGhostObject(this);
             if (this.hex.hexgrid == ghosthex.hexgrid)
@@ -121,7 +146,7 @@ namespace spaceconquest
                 Console.WriteLine("creating new linemodel");
                 line = new LineModel(getCenter(), ghosthex.getCenter());
             }
-        } 
+        }
 
         public override void Draw(Microsoft.Xna.Framework.Matrix world, Microsoft.Xna.Framework.Matrix view, Microsoft.Xna.Framework.Matrix projection)
         {
@@ -131,7 +156,8 @@ namespace spaceconquest
 
             //update current angle, and hmove it closer to the target angle,
             double angleIncr = (Math.PI / 25.0);
-            if (Math.Abs( currentAngle - targetangle) > (Math.PI/20.0) )
+
+            if (Math.Abs(currentAngle - targetangle) > (Math.PI / 20.0))
             {
                 //Then we need to modify depending on angle difference
                 if (currentAngle < targetangle)
@@ -142,11 +168,17 @@ namespace spaceconquest
                 {
                     currentAngle -= angleIncr;
                 }
-           }
+            }
+            else
+            {
+                if (targetangles.Count() != 0)
+                {
+                    if (percenttraveled == 100) targetangle = targetangles.Dequeue();
+                }
+            }
 
-            
             if (percenttraveled < 100) { percenttraveled = percenttraveled + 4; }
-            if (percenttraveled >= 100) 
+            else
             {
                 if (targetpositions.Count() != 0)
                 {
@@ -159,7 +191,7 @@ namespace spaceconquest
             }
             //Console.WriteLine("percenttaveled :: {0} ", percenttraveled);
 
-            Vector3 currentvector = (targetvector - oldposition)*(percenttraveled/100.0f) + oldposition;
+            Vector3 currentvector = (targetvector - oldposition) * (percenttraveled / 100.0f) + oldposition;
             //Console.WriteLine("current vector :: {0} \n target vector :: {1} ", currentvector, targetvector);
 
             //Create translation gets hex world coordinates 
